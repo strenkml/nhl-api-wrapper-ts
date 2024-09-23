5424f287e0a37a9f75f53c0690ec52f448f38056
default <mstrenk12@gmail.com>
2024-09-23 14:54:40 +0000

    More restructuring

--- src/api/players/Players.ts
+++ src/api/players/Players.ts
@@ -1,47 +1,9 @@
-import {
-  IPlayerCurrentGameLogInput,
-  IPlayerCurrentGameLogOutput,
-  IPlayerGameLogInput,
-  IPlayerGameLogOutput,
-  IPlayerInfoInput,
-  IPlayerInfoOutput,
-} from "../../interfaces/Players";
-import { BASE_URL, IResponse, sendGetRequest } from "../../internal/Requests";
-
+import * as generic from "./generic/Generic";
 import * as skaters from "./skaters/Skaters";
-import * as goalies from "./goalies/Goalies";
-
-/**
- * Retrieve the game log for a specific player, season, and game type.
- * @param options The player ID and season and game type to retrieve the game log for.
- * @returns The game log for the player.
- */
-async function getPlayerGameLog(options: IPlayerGameLogInput): Promise<IResponse<IPlayerGameLogOutput>> {
-  return await sendGetRequest<IPlayerGameLogOutput>(BASE_URL.NORMAL, "player/{playerId}/game-log/{season}/{gameType}", options);
-}
-
-/**
- * Retrieve the player information for a specific player.
- * @param options The player ID to retrieve the player information for.
- * @returns The player information for the player.
- */
-async function getPlayerInfo(options: IPlayerInfoInput): Promise<IResponse<IPlayerInfoOutput>> {
-  return await sendGetRequest<IPlayerInfoOutput>(BASE_URL.NORMAL, "player/{playerId}/landing", options);
-}
-
-/**
- * Retrieve the game log for a specific player as of the current moment.
- * @param options The player ID to retrieve the game log for.
- * @returns The current game log for the player.
- */
-async function getPlayerCurrentGameLog(options: IPlayerCurrentGameLogInput): Promise<IResponse<IPlayerCurrentGameLogOutput>> {
-  return await sendGetRequest<IPlayerCurrentGameLogOutput>(BASE_URL.NORMAL, "player/{playerId}/game-log/now", options);
-}
+// import * as goalies from "./goalies/Goalies";
 
 export default {
-  getPlayerGameLog,
-  getPlayerInfo,
-  getPlayerCurrentGameLog,
+  generic,
   skaters,
-  goalies,
+  // goalies,
 };
---
+++ src/api/players/generic/Generic.ts
@@ -0,0 +1,36 @@
+import {
+  IPlayerCurrentGameLogInput,
+  IPlayerCurrentGameLogOutput,
+  IPlayerGameLogInput,
+  IPlayerGameLogOutput,
+  IPlayerInfoInput,
+  IPlayerInfoOutput,
+} from "../../../interfaces/Players";
+import { BASE_URL, IResponse, sendGetRequest } from "../../../internal/Requests";
+
+/**
+ * Retrieve the game log for a specific player, season, and game type.
+ * @param options The player ID and season and game type to retrieve the game log for.
+ * @returns The game log for the player.
+ */
+export async function getPlayerGameLog(options: IPlayerGameLogInput): Promise<IResponse<IPlayerGameLogOutput>> {
+  return await sendGetRequest<IPlayerGameLogOutput>(BASE_URL.NORMAL, "player/{playerId}/game-log/{season}/{gameType}", options);
+}
+
+/**
+ * Retrieve the player information for a specific player.
+ * @param options The player ID to retrieve the player information for.
+ * @returns The player information for the player.
+ */
+export async function getPlayerInfo(options: IPlayerInfoInput): Promise<IResponse<IPlayerInfoOutput>> {
+  return await sendGetRequest<IPlayerInfoOutput>(BASE_URL.NORMAL, "player/{playerId}/landing", options);
+}
+
+/**
+ * Retrieve the game log for a specific player as of the current moment.
+ * @param options The player ID to retrieve the game log for.
+ * @returns The current game log for the player.
+ */
+export async function getPlayerCurrentGameLog(options: IPlayerCurrentGameLogInput): Promise<IResponse<IPlayerCurrentGameLogOutput>> {
+  return await sendGetRequest<IPlayerCurrentGameLogOutput>(BASE_URL.NORMAL, "player/{playerId}/game-log/now", options);
+}
